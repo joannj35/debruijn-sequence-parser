f473617580ce71111bff7ca011d9f5f543f60db9
Joan Jozen <52244617+joannj35@users.noreply.github.com>
2023-12-21 22:33:23 +0200

    handle corrupt file exception

--- summaryScript/Program.cs
+++ summaryScript/Program.cs
@@ -34,33 +34,41 @@ namespace parserExtentions
 
             GetValueBasedOnRegex(fileContent, @"complexity (\d+):", out int complexity);
 
-            //The information about sequences is written into three separate files: one for sequences that yield DeBruijn sequences, one for sequences that don't, and a summary file.
-            using (StreamWriter nonZeroWriter = File.AppendText(Path.Combine(directoryPath, $"{complexity}_yielding_small_sequences.txt")))
-            using (StreamWriter zeroWriter = File.AppendText(Path.Combine(directoryPath, $"{complexity}_non_yielding_small_sequences.txt")))
-            using (StreamWriter summaryWriter = File.AppendText(Path.Combine(directoryPath, $"{complexity}_summary.txt")))
+            try
             {
-                foreach (Match match in Regex.Matches(fileContent, pattern))
-                    summaryWriter.WriteLine(match.Value);
+                //The information about sequences is written into three separate files: one for sequences that yield DeBruijn sequences, one for sequences that don't, and a summary file.
+                using (StreamWriter nonZeroWriter = File.AppendText(Path.Combine(directoryPath, $"{complexity}_yielding_small_sequences.txt")))
+                using (StreamWriter zeroWriter = File.AppendText(Path.Combine(directoryPath, $"{complexity}_non_yielding_small_sequences.txt")))
+                using (StreamWriter summaryWriter = File.AppendText(Path.Combine(directoryPath, $"{complexity}_summary.txt")))
+                {
+                    foreach (Match match in Regex.Matches(fileContent, pattern))
+                        summaryWriter.WriteLine(match.Value);
 
-                GetSmallSeq(fileContent, out List<string> yieldDb, out List<string> nonYieldDb);
+                    GetSmallSeq(fileContent, out List<string> yieldDb, out List<string> nonYieldDb);
 
-                summaryWriter.WriteLine($"The total number of sequences of small complexity={complexity} which yield debruijn sequences is: {yieldDb.Count}\n"
-                              + $"The total number of sequences of small complexity={complexity} which DO NOT yield any debruijn sequences is: {nonYieldDb.Count}");
+                    summaryWriter.WriteLine($"The total number of sequences of small complexity={complexity} which yield debruijn sequences is: {yieldDb.Count}\n"
+                                  + $"The total number of sequences of small complexity={complexity} which DO NOT yield any debruijn sequences is: {nonYieldDb.Count}");
 
-                nonZeroWriter.WriteLine($"Summary of sequences of small complexity={complexity} which yield debruijn sequences:\n"
-                    + $"Total number of sequences in this file: {yieldDb.Count}");
-                foreach (var seq in yieldDb)
-                    nonZeroWriter.WriteLine($"{seq}");
+                    nonZeroWriter.WriteLine($"Summary of sequences of small complexity={complexity} which yield debruijn sequences:\n"
+                        + $"Total number of sequences in this file: {yieldDb.Count}");
+                    foreach (var seq in yieldDb)
+                        nonZeroWriter.WriteLine($"{seq}");
 
-                zeroWriter.WriteLine($"Summary of sequences of small complexity={complexity} which DO NOT yield debruijn sequences:\n"
-                    + $"Total number of sequences in this file: {nonYieldDb.Count}");
-                foreach (var seq in nonYieldDb)
-                    zeroWriter.WriteLine($"{seq}");
+                    zeroWriter.WriteLine($"Summary of sequences of small complexity={complexity} which DO NOT yield debruijn sequences:\n"
+                        + $"Total number of sequences in this file: {nonYieldDb.Count}");
+                    foreach (var seq in nonYieldDb)
+                        zeroWriter.WriteLine($"{seq}");
 
-                // sanity check
-                GetValueBasedOnRegex(fileContent, @"of small complexity \d+ is: (\d+)", out int total);
-                if (nonYieldDb.Count + yieldDb.Count != total)
-                    throw new Exception("Sanity check failed!");
+                    // sanity check
+                    GetValueBasedOnRegex(fileContent, @"of small complexity \d+ is: (\d+)", out int total);
+                    if (nonYieldDb.Count + yieldDb.Count != total)
+                        throw new Exception($"Sanity check for complexity {complexity} has failed! No summary files will be created.");
+                }
+            }
+            catch (Exception ex)
+            {
+                DeleteFilesForComplexity(directoryPath, complexity);
+                Console.WriteLine($"Error: {ex.Message}");
             }
         }
 
@@ -90,6 +98,17 @@ namespace parserExtentions
                     nonYieldDb.Add(match.Groups[1].Value);
                 else
                     yieldDb.Add(match.Groups[1].Value);
+            }
+        }
+        /// <summary>
+        /// in case of an error, the corrupted files of the specified complexities are deleted
+        /// </summary>
+        static void DeleteFilesForComplexity(string directoryPath, int complexity)
+        {
+            var filesToDelete = Directory.GetFiles(directoryPath, $"{complexity}_*.txt");
+            foreach (var file in filesToDelete)
+            {
+                File.Delete(file);
             }
         }
     }
